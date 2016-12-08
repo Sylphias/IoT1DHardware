fdc2079128c82a27493b23ce96b2977fd9b950e9
Sylphias <sylpharyon@gmail.com>
2016-12-09 02:23:16 +0800

    Fixed api requester class, now need to fix Microsoft API

--- src/HttpRequests/HttpRequester.java
+++ src/HttpRequests/HttpRequester.java
@@ -3,6 +3,10 @@ package HttpRequests;
 import IoTData.EmotionData;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.*;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -10,11 +14,25 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import Utilities.APIKeys;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 /**
  * Created by Ilya on 7/11/16.
  *
@@ -66,41 +84,109 @@ public class HttpRequester {
         return null;
     }
 
-    public static String generalRequester(String targetURL,String urlParameters,String data, String requestType){
-        HttpURLConnection connection = null;
+//    public static String generalRequester(String targetURL,String urlParameters,String data, String requestType){
+//        HttpURLConnection connections = null;
+//        try{
+//            String fullURL = targetURL;
+//            if(urlParameters != null){
+//                fullURL += urlParameters;
+//            }
+//            URL url = new URL(fullURL);
+//            connections = (HttpURLConnection) url.openConnection();
+//            connections.setRequestProperty("Connection","keep-alive");
+//            connections.setRequestProperty("Content-Type", "application/json");
+////            connections.setRequestProperty("Method", requestType);
+//            connections.setRequestProperty("Host", targetURL);
+//            connections.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.28 (KHTML, like Gecko) Version/3.2.2 Safari/525.28.1");
+//            connections.setUseCaches(false);
+//            connections.setDoOutput(true);
+//            connections.setRequestMethod(requestType);
+//            //The Following Sends the data stream as a request
+//            if((requestType == "POST" || requestType =="PUT") && data != "") {
+//                DataOutputStream wr = new DataOutputStream(connections.getOutputStream());
+//                wr.writeBytes(data);
+//                wr.close();
+//            }
+//            InputStream is = connections.getInputStream();
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+//            StringBuilder response = new StringBuilder();
+//            String line;
+//            while((line = rd.readLine()) != null ){
+//                response.append(line);
+//            }
+//            rd.close();
+//            return response.toString();
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//        return null;
+//    }
+
+    public static String generalRequester(String targetURL, String path, HashMap<String,String> urlParameters, String data, String requestType){
+        HttpURLConnection connections = null;
+        String responseEntity =null;
         try{
-            if(urlParameters != null){
-                targetURL += urlParameters;
+            URIBuilder uri = new URIBuilder()
+                    .setScheme("http")
+                    .setHost(targetURL)
+                    .setPath(path);
+            for (HashMap.Entry<String,String> params: urlParameters.entrySet()){
+                uri.addParameter(params.getKey(),params.getValue());
             }
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Method", requestType);
-            connection.setRequestProperty("User-Agent", "Mozilla/4.76");
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
+            uri.build();
 
+            HttpClient client = HttpClientBuilder.create().build();
             //The Following Sends the data stream as a request
+            if((requestType == "POST" || requestType =="PUT") && data != "") {
+                HttpPost hp = new HttpPost(String.valueOf(uri));
+                List<NameValuePair> urlParams = new ArrayList<NameValuePair>();
+                for (HashMap.Entry<String,String> params : urlParameters.entrySet()){
+                    urlParams.add(new BasicNameValuePair(params.getKey(),params.getValue()));
+                }
+                hp.setEntity(new UrlEncodedFormEntity(urlParams));
+                HttpResponse response = client.execute(hp);
+                System.out.println("Response Code : "
+                        + response.getStatusLine().getStatusCode());
 
-            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-            if(requestType == "POST" || requestType =="PUT") {
-                wr.writeBytes(data);
+                BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer result = new StringBuffer();
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+                return result.toString();
             }
-            wr.close();
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while((line = rd.readLine()) != null ){
-                response.append(line);
+            else if (requestType == "GET"){
+                HttpGet hg = new HttpGet(String.valueOf(uri));
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                CloseableHttpResponse response = httpclient.execute(hg);
+                try {
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null) {
+                        long len = entity.getContentLength();
+                        if (len != -1 && len < 2048) {
+                            //Entities can only be read once
+                            responseEntity = EntityUtils.toString(entity);
+                        }else {
+                            InputStream instream = entity.getContent();
+                        }
+                    }
+                } finally {
+                    response.close();
+                }
+                return responseEntity;
             }
-            rd.close();
-            return response.toString();
         }catch(Exception e){
             System.out.println(e);
         }
-        return null;
+        return responseEntity;
     }
+
+
+// Started GET "/person/by_name?name=Ilya" for 202.94.70.25 at 2016-12-07 18:54:08 +0000
+// Started GET "/person/by_name?name=Ilya" for 202.94.70.51 at 2016-12-07 19:10:18 +0000
 
     public static String parameterfier(HashMap<String,String> params){
         StringBuilder sb = new StringBuilder();
@@ -110,6 +196,46 @@ public class HttpRequester {
         }
         return sb.toString();
     }
+
+    public static HashMap<String,String> objectifier(String json){
+        Gson gson = new Gson();
+        Type type = new TypeToken<HashMap<String, String>>() {
+        }.getType();
+        HashMap<String, String> data = gson.fromJson(json, type);
+        return data;
+    }
+
+
+//    public static String responseHandler(){
+//        CloseableHttpClient httpclient = HttpClients.createDefault();
+//        HttpGet httpget = new HttpGet("http://localhost/json");
+//
+//        ResponseHandler<MyJsonObject> rh = new ResponseHandler<MyJsonObject>() {
+//
+//            @Override
+//            public JsonObject handleResponse(
+//                    final HttpResponse response) throws IOException {
+//                StatusLine statusLine = response.getStatusLine();
+//                HttpEntity entity = response.getEntity();
+//                if (statusLine.getStatusCode() >= 300) {
+//                    throw new HttpResponseException(
+//                            statusLine.getStatusCode(),
+//                            statusLine.getReasonPhrase());
+//                }
+//                if (entity == null) {
+//                    throw new ClientProtocolException("Response contains no content");
+//                }
+//                Gson gson = new GsonBuilder().create();
+//                ContentType contentType = ContentType.getOrDefault(entity);
+//                Charset charset = contentType.getCharset();
+//                Reader reader = new InputStreamReader(entity.getContent(), charset);
+//                return gson.fromJson(reader, MyJsonObject.class);
+//            }
+//        };
+//        MyJsonObject myjson = client.execute(httpget, rh);
+//
+//        return "";
+//    }
 
 
 }
--- src/Main/BackgroundRunner.java
+++ src/Main/BackgroundRunner.java
@@ -42,18 +42,21 @@ public class BackgroundRunner {
         Gson gson = new Gson();
         int i = 0;
         String id = System.getProperty("user.name");
-
-        String personResponse = HttpRequester.generalRequester("http://iotfocus.herokuapp.com/person/by_name","?name="+id,"","GET");
+        HashMap<String,String> personParams = new HashMap<String, String>();
+        personParams.put("name",id);
+        String personResponse = HttpRequester.generalRequester("iotfocus.herokuapp.com/","person/by_name",personParams,"","GET");
         Person pers = null;
-        if(personResponse != null) {
+        if(!personResponse.matches("null")) {
             pers = new Person(personResponse);
         }
+        // If we cannot find a person we will create a person
         else{
             // Create a new person.
-            Person p = new Person();
-            p.setName(id);
-            String request = HttpRequester.generalRequester("http://iotfocus.herokuapp.com/person","?name="+id,"","POST");
+            Person p = new Person(18,id,'U');
+            String pjson = gson.toJson(p);
+            String request = HttpRequester.generalRequester("iotfocus.herokuapp.com","/person",p.toHashMap(),pjson,"POST");
             System.out.println(request);
+            pers = p;
         }
         try {
             grab.start();
@@ -76,8 +79,10 @@ public class BackgroundRunner {
                     }
                     if (pers != null) {
                        EmotionData ed = HttpRequester.emotionRequester("{\"url\":\"" + URL + "\"}", "POST", pers.getId());
-                        String result = HttpRequester.generalRequester("http://iotfocus.herokuapp.com/emotiondatum",HttpRequester.parameterfier(ed.toHashMap()),"","POST");
-                        System.out.println(result);
+                        if( ed != null) {
+                            String result = HttpRequester.generalRequester("iotfocus.herokuapp.com", "emotiondatum", ed.toHashMap(), "", "POST");
+                            System.out.println(result);
+                        }
                     }
                 }
                 Thread.sleep(5000);
